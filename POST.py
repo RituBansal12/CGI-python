57ff37222a2bd8335d35dfd2fd69f75abe7f5ddf
RituBansal12 <75537428+RituBansal12@users.noreply.github.com>
2021-02-02 15:55:24 +0530

    Update POST.py

--- POST.py
+++ POST.py
@@ -9,9 +9,9 @@ num_1 = form.getvalue('num_1')
 num_2  = form.getvalue('num_2')
 n1= int(num_1)
 n2= int(num_2)
-sum = n1 + n2
-sub= n1 - n2
-Mul= n1* n2
+add = n1 + n2
+subt= n1 - n2
+mul= n1* n2
 div= n1/n2
 
 print "<html>"
@@ -19,9 +19,9 @@ print "<head>"
 print "<title>MATH OPERATIONS</title>"
 print "</head>"
 print "<body>"
-print "<h2>SUM %d</h2>" % (sum)
-print "<h2>MINUS %d</h2>" % (sub)
-print "<h2>PRODUCT %d</h2>" % (Mul)
+print "<h2>SUM %d</h2>" % (add)
+print "<h2>MINUS %d</h2>" % (subt)
+print "<h2>PRODUCT %d</h2>" % (mul)
 print "<h2>DIVISION %d</h2>" % (div)
 print "</body>"
 print "</html>"
